3a3cbf90d9f534ea6d2545a831df132a3a44a9d1
Ravi Ram <raviram220204@gmail.com>
2025-07-28 23:59:03 +0530

    Remove s from logo

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -5,7 +5,7 @@ const Logo = () => {
     <div className="flex items-center space-x-1 text-2xl sm:text-3xl font-bold select-none leading-none">
       <span className="text-gray-900 tracking-tight">Hostel</span>
       <span className="relative text-primary font-bold">
-        Bites
+        Bite
         <span className="absolute -bottom-1 left-0 w-full h-[2px] bg-primary-dull rounded-sm"></span>
       </span>
     </div>
